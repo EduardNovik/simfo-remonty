d0a47effd042b004e9af87155ce494a0fab82881
Eduard Novik <eddnovik@gmail.com>
2024-03-10 17:46:02 +0100

    small fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Layout from "@/components/layout";
 
 const About = () => {
-  return <Layout>About</Layout>;
+  return <Layout>About us</Layout>;
 };
 
 export default About;
